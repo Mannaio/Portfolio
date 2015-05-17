3bfaa8a9d3ffa0737f0e86a2e4bf62e2af092cc3
Mannaio <info@tommasovilla.com>
2015-05-17 13:41:37 +0200

    Navigation Bar

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,10 +3,9 @@ import Ember from 'ember';
 var ApplicationRoute = Ember.Route.extend({
     renderTemplate: function() {
         this._super();
-        return this.render('player', {
+        return this.render('menu', {
             into: 'application',
-            outlet: 'player',
-            controller: 'player'
+            outlet: 'menu',
         });
     }
 });
