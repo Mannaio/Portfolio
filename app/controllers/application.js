6214b6d89327a5b62e62cc5328276db3e0b1d330
Mannaio <info@tommasovilla.com>
2015-06-12 20:24:47 +0200

    Menu Component Basic Functionality and Style

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 var ApplicationController = Ember.Controller.extend({
     artistName: null,
+    showMenu: true,
     actions: {
         goToArtist: function() {
             var artist;
@@ -9,7 +10,10 @@ var ApplicationController = Ember.Controller.extend({
                 artist = this.get('artistName');
                 return this.transitionToRoute('artist', artist);
             }
-        }
+        },
+        showMenu: function() {
+            this.set("showMenu", !this.get("showMenu"));
+        },
     }
 });
 
