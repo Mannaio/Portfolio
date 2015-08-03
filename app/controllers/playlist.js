a438607d5df231d5d5dd17f53b3148737b7c7808
Mannaio <info@tommasovilla.com>
2015-08-04 00:17:22 +0200

    Replace with standard image if model is null

--- app/controllers/playlist.js
+++ app/controllers/playlist.js
@@ -20,14 +20,16 @@ var PlaylistController = Ember.ObjectController.extend({
         }
         return trackArt;
     }),
-    formattedArtwork: Ember.computed('albumArt', function() {
+    formattedArtwork: Ember.computed('artwork_url', function() {
         var splitURL, url;
-        if (this.get('albumArt')) {
-            url = this.get('albumArt');
+        if (this.get('artwork_url')) {
+            url = this.get('artwork_url');
             splitURL = url.split('-large');
             return splitURL[0] + '-t500x500' + splitURL[1];
+        } else {
+            return "https://i1.sndcdn.com/artworks-000121961221-bzjnxn-t500x500.jpg";
         }
-    })
+    }),
 });
 
 export default PlaylistController;
