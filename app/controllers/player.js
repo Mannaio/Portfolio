9fbade7f2cf94a18bf5a6f63033b5ac0d3a48f4e
Mannaio <info@tommasovilla.com>
2015-09-27 00:33:23 +0200

    SoundCloud Player music streaming favorites

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -23,42 +23,42 @@ var PlayerController = Ember.Controller.extend({
             if (play == null) {
                 play = true;
             }
-            // self.get('favorites').setEach('playingFavorite', false);
+            self.get('favorite').set('playingFavorite', false);
             // self.set('isBuffering', true);
             favorite.set('playingFavorite', true);
             favoritePath = favorite.get('uri');
-            // prevFavorite = this.get('currentFavoriteObject');
-            // if (prevFavorite != null) {
-            //     prevFavorite.destruct();
-            // }
+            prevFavorite = this.get('currentFavoriteObject');
+            if (prevFavorite != null) {
+                prevFavorite.destruct();
+            }
             self.set('currentFavorite', favorite);
             // if (!this.get('externalPlay')) {
             //     index = index + 1;
             //     nextFavorite = this.get('sortedFavorites').nextObject(index, favorite);
             //     self.set('nextFavorite', nextFavorite);
             // }
-            // return SC.stream(FavoritePath, {
-            //     whileplaying: function() {
-            //         return self.set('currentFavoritePosition', this.position);
-            //     },
-            //     onbufferchange: function() {
-            //         return self.set('isBuffering', this.isBuffering);
-            //     },
-            //     onfinish: function() {
-            //         self.set('isPlaying', false);
-            //         if (self.get('nextFavorite') != null) {
-            //             return self.send('selectFavorite', self.get('nextFavorite'), index);
-            //         }
-            //     }
-            // }, function(sound) {
-            //     self.set('currentFavoriteObject', sound);
-            //     self.set('isPlaying', true);
-            //     sound.play();
-            //     if (!play) {
-            //         sound.pause();
-            //         return self.set('isPlaying', false);
-            //     }
-            // });
+            return SC.stream(favoritePath, {
+                whileplaying: function() {
+                    return self.set('currentFavoritePosition', this.position);
+                },
+                onbufferchange: function() {
+                    return self.set('isBuffering', this.isBuffering);
+                },
+                onfinish: function() {
+                    self.set('isPlaying', false);
+                    if (self.get('nextFavorite') != null) {
+                        return self.send('selectFavorite', self.get('nextFavorite'), index);
+                    }
+                }
+            }, function(sound) {
+                self.set('currentFavoriteObject', sound);
+                self.set('isPlaying', true);
+                sound.play();
+                if (!play) {
+                    sound.pause();
+                    return self.set('isPlaying', false);
+                }
+            });
         }
     }
 });
