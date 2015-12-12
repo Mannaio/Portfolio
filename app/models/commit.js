1f230b20bfedb8686cb7ebc7fa42fd205cb9bd83
Mannaio <info@tommasovilla.com>
2015-12-12 20:21:32 +0100

    Commit Message retrieve

--- app/models/commit.js
+++ app/models/commit.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 
 var commit = DS.Model.extend({
-	message: DS.attr('string')
+	message: DS.attr('string'),
+	url: DS.attr('string')
 });
 
 export default commit;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,70 @@
-import ajax from 'ic-ajax';
 import Ember from 'ember';
 
 var IndexRoute = Ember.Route.extend({
     visibility: Ember.inject.service('visible'),
     model: function() {
-        var store, userUrl, reposUrl, reposAct, eventsAct;
+        var store, userUrl, reposUrl, eventsAct, gituser;
         store = this.get('store');
         userUrl = 'https://api.github.com/users/mannaio';
         reposUrl = 'https://api.github.com/users/mannaio/repos';
         eventsAct = 'https://api.github.com/users/mannaio/events';
+
+        // var gituserPromise = function() {
+        //     return new Ember.RSVP.Promise(function(resolve, reject) {
+        //         return Ember.$.getJSON(userUrl, function(data) {
+        //             var item = [];
+        //             gituser.setProperties({
+        //                 name: data.name,
+        //                 login: data.login,
+        //                 location: data.location,
+        //                 company: data.company,
+        //                 followers: data.followers,
+        //                 following: data.following
+        //             });
+        //             item.pushObject(gituser);
+        //             return resolve(item);
+        //         });
+        //     });
+        // };
+
+        // var gituserPromise = function() {
+        //     return new Ember.RSVP.Promise(function (resolve) {
+        //         return Ember.$.ajax(userUrl, function(data) {
+        //             var item = [];
+        //             gituser.setProperties({
+        //                 name: data.name,
+        //                 login: data.login,
+        //                 location: data.location,
+        //                 company: data.company,
+        //                 followers: data.followers,
+        //                 following: data.following,
+        //                 email: data.email
+        //             });
+        //             item.pushObject(gituser);
+        //             return resolve(item);
+        //         });
+        //     });
+        // };
+
+
+        // var gituserPromise = function() {
+        //     return new Ember.RSVP.Promise(function (resolve) {
+        //         return Ember.$.ajax(userUrl, {
+        //             success: function(data) {               
+        //                 var item = [];
+        //                 item.push(store.createRecord('gituser', {
+        //                     name: data.name,
+        //                     login: data.login,
+        //                     email: data.email,
+        //                 }));
+        //                 resolve(item);
+        //            },
+        //             error: function(reason) {
+        //              reject(reason);
+        //            }});
+        //     });
+        // };
+
 
         var gituserPromise = function() {
             return Ember.$.ajax(userUrl, {
@@ -29,20 +85,50 @@ var IndexRoute = Ember.Route.extend({
                  reject(reason);
                }});
         };
+
         var gitrepositoriesPromise = function() {
-            return Ember.$.ajax(reposUrl, {
-                success: function(repos) {
-                    return repos.map(function(repo) {
-                        return store.createRecord('repo', {
-                            name: repo.name,
-                            description: repo.description,
-                            language: repo.language
+            return new Ember.RSVP.Promise(function (resolve, reject) {
+                return Ember.$.ajax(reposUrl, {
+                    success: function(repos) {
+                        var result = [];
+                        repos.map(function(repo) {
+                            result.push(store.createRecord('repo', {
+                                name: repo.name,
+                                description: repo.description,
+                                language: repo.language
+                            }));
+                        });
+                        resolve(result);
+                    },
+                    error: function(reason) {
+                     reject(reason);
+                    },
+                }); 
+            });            
+        };
+       
+        var gitactivitiesPromise = function() {
+            return new Ember.RSVP.Promise(function (resolve, reject) {
+                Ember.$.ajax(eventsAct, {
+                    success: function(events) {
+                        var result = [];
+                        events.filter(function(event) {
+                    return event.type === 'PushEvent';
+                    }).forEach(function(item){
+                        item.payload.commits.map(function(commit){
+                            result.push(store.createRecord('commit', {
+                                message: commit.message,
+                                url: commit.url
+                            }));
                         });
                     });
-                },
-                error: function(reason) {
-                 reject(reason);
-            }});             
+                    resolve(result);
+                  },  
+                  error: function(reason) {
+                    reject(reason);
+                  }
+                });
+            });
         };
         // var gitactivitiesPromise = function() {
         //     return Ember.$.ajax(eventsAct, {
@@ -51,37 +137,17 @@ var IndexRoute = Ember.Route.extend({
         //                 return event.type == 'PushEvent';
         //             }).forEach(function(item){
         //                 return item.payload.commits.map(function(commit){
-        //                     // return console.log(commit.message);
-        //                     return commit.message;
+        //                     return store.createRecord('commit', {
+        //                         message: commit.message,
+        //                     });
         //                 });
         //             });
-        //             // return events.map(function(event) {
-        //             //     return store.createRecord('event', {
-        //             //         name: event.type
-        //             //     });
-        //             // });
-        //         },
+        //         },  
         //         error: function(reason) {
         //          reject(reason);
         //     }});             
         // };
-        var gitactivitiesPromise = function() {
-            return Ember.$.ajax(eventsAct, {
-                success: function(events) {
-                    return events.filter(function(event) {
-                        return event.type == 'PushEvent';
-                    }).forEach(function(item){
-                        return item.payload.commits.map(function(commit){
-                            return store.createRecord('commit', {
-                                message: commit.message,
-                            });
-                        });
-                    });
-                },  
-                error: function(reason) {
-                 reject(reason);
-            }});             
-        };
+        
         return Ember.RSVP.hash({
             gitUsers: gituserPromise(),
             repos: gitrepositoriesPromise(),
