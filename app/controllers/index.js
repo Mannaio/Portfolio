7f6a145bff85eb9e3b6d24b3992986b7ece40c5a
Mannaio <info@tommasovilla.com>
2015-12-21 00:50:42 +0100

    Added Image Index Page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 var IndexController = Ember.Controller.extend({
-    errorText: null
+    errorText: null,
+    avatarUrl: 'http://s27.postimg.org/brwuyr0r7/GOPR0392.jpg'
 });
 
 export default IndexController;
