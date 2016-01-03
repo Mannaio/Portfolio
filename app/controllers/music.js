d1a39943292b9c1363cfcaccd84779988f17a50d
Mannaio <info@tommasovilla.com>
2016-01-03 16:35:23 +0100

    Added Controller sorting Playlist

--- app/controllers/music.js
+++ app/controllers/music.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 var MusicController = Ember.ArrayController.extend({
     needs: ['player'],
     player: Ember.computed.alias('controllers.player'),
+    sortProperties: ['created_at:desc'],   
+    sortedFavorites: Ember.computed.sort('model', 'sortProperties'),
     actions: {
         setAsFavorite: function (favorite) {
             var favorites = this.get('model');
